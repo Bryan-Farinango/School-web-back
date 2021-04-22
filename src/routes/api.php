44261c3379809bc995dfbaf2e0a6f889d81b3ef2
Josselyn Vela <deojosselyn@hotmail.com>
2021-04-21 22:18:03 -0500

    creacion api role y api asignaturas JV

--- src/routes/api.php
+++ src/routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('registro', 'ApiRegisterController@userRegister');
+Route::post('roles', 'ApiRolesController@userRol');
+Route::post('asignaturas', 'ApiSubjectsController@subjects');
 
 // Route::post('emitir_simple',  function (Request $request){
 //     return json_encode(array("result" => "si"));
