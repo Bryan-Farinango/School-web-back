58f5d46e684b713f42df61d0c3ae1bd8c285fadf
Josselyn Vela <deojosselyn@hotmail.com>
2021-05-21 16:53:46 -0500

    creacion api publicaciones

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('registro', 'ApiRegisterController@userRegister');
 Route::post('roles', 'ApiRolesController@userRol');
 Route::post('asignaturas', 'ApiSubjectsController@subjects');
+Route::post('publicaciones', 'ApiPublicationsController@publications');
 
 // Route::post('emitir_simple',  function (Request $request){
 //     return json_encode(array("result" => "si"));
