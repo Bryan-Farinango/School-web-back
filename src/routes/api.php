9b368d2cfcf94d47ecec7b086abd16af52079261
Bryan-Farinango <bryan.farinango@epn.edu.ec>
2021-08-28 19:43:58 -0500

    BF add transporte

--- src/routes/api.php
+++ src/routes/api.php
@@ -45,7 +45,7 @@ Route::post('delete-driver-from-ruta', 'ApiAdminController@deleteDriver');
 Route::post('add-driver-to-ruta', 'ApiAdminController@addDriver');
 Route::post('update-rutas', 'ApiAdminController@updateAllDriver');
 Route::post('delete-rutas', 'ApiAdminController@deleteRuta');
-Route::post('get-rutas-for-user', 'ApiAdminController@getRutasforUser');
+Route::post('get-rutas-for-user', 'ApiRegisterController@getRutasforUser');
 //estudiantes
 Route::post('add-estudiantes', 'ApiRegisterController@estudiantes');
 Route::post('get-estudiantes', 'ApiRegisterController@getEstudiantesInfo');
