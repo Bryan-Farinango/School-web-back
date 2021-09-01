455241272845543b73dbb36c1a80f9f5f13f936a
Bryan-Farinango <bryan.farinango@epn.edu.ec>
2021-08-31 23:07:21 -0500

    BF add transporte

--- src/routes/api.php
+++ src/routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
 Route::post('user-info', 'ApiRegisterController@getUserInfo');
 Route::post('login-user', 'ApiRegisterController@loginUser');
 Route::post('registro', 'ApiRegisterController@userRegister');
